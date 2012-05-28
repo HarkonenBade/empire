95196b28cadf48804e6ebfe1985f8ee40a6e4c0b
Tom Bytheway <tb403@cam.ac.uk>
2012-05-28 17:36:37 +0100

    Initial

---
+++ com/github/tomb3/empire/empire.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.github.tomb3.empire;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author tomb3
+ *
+ */
+public class empire extends JavaPlugin{
+
+	public void onEnable(){ 
+		 
+	}
+	 
+	public void onDisable(){ 
+	 
+	}
+}
